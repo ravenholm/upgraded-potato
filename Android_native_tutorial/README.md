9f5de87b214bf8fdeea908f4baf2c4b83cb4884c
ravenholm1 <adnan.yusuf@gmail.com>
2020-06-27 04:44:23 +0300

    Android camera capture and OpenCV integration working

--- Android_native_tutorial/README.md
+++ Android_native_tutorial/README.md
@@ -24,4 +24,117 @@ sudo chmod 777 -R studio.sh
 mv opencv-4.3.0-android-sdk.zip ~/
 unzip opencv-4.3.0-android-sdk.zip
 ```
-3. Install Gradle's most recent release [Check from here](https://gradle.org/releases/)
+3. Install/upgrade your Gradle build tools [Releases](https://gradle.org/releases/)
+
+* Switch to the Android view
+* Go to your **build.gradle** for your **module**  
+```java
+  // opencv 4.1.0
+  implementation 'com.quickbirdstudios:opencv:4.1.0'
+  // opencv 4.1.0 with contribution packages
+  implementation 'com.quickbirdstudios:opencv:4.1.0-contrib'
+```
+**In the same build.gradle(:app), change the minimumSDK to 21 to comply with com.quickbirdstudios:opencv:4.1.0**
+4. Pull OpenCV with NDK configuration
+This section is taken from [this link](https://github.com/quickbirdstudios/opencv-android)
+
+* Go to your **build.gradle** for your Project: <whatever your project name> 
+```java
+    dependencies {
+        //Change the following to 
+        // classpath "com.android.tools.build:gradle:4.0.0"
+        classpath "com.android.tools.build:gradle:6.4.1"
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+```
+* Now, sync your gradle
+
+# Actual development
+
+Open your MainActivity file and implement the following:
+```java
+    public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 
+```
+1. Now add/implement all the required methods (there are three methods)
+2. Declare the following in your MainActivity class
+```java
+    private static final String TAG = "myTag";
+    JavaCameraView javaCameraView;
+
+    private BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this) {
+            @Override
+            public void onManagerConnected(int status) {
+    
+                switch (status){
+                    case LoaderCallbackInterface.SUCCESS:
+                        javaCameraView.enableView();
+                        Log.d(TAG, "callback: opencv loaded successfully");
+                        break;
+                    default:
+                        Log.d(TAG, "could not load opencv");
+    
+    
+                }
+    
+    
+            }
+        };
+```
+3. Override the OnCreate as follows:
+```java
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        javaCameraView = (JavaCameraView) findViewById(R.id.java_camera_view);//this will still be red
+        javaCameraView.setVisibility(View.VISIBLE);
+        javaCameraView.setCvCameraViewListener(this);
+```
+4. Override the OnResume as follows:
+
+```java
+        @Override
+        protected void onResume(){
+            Log.d(TAG, "in On Resume");
+            super.onResume();
+    
+            if (!OpenCVLoader.initDebug()){
+                boolean success = OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallBack);
+    
+                if (success){
+                    Log.d(TAG, "Asynchronous initialisation succeeded");
+                }
+                else{
+                    Log.d(TAG, "Asynchronous initialisation failed");
+                }
+            }
+            else{
+    
+                Log.d(TAG, "OpenCV library found inside package");
+                mLoaderCallBack.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+            }
+        }
+```
+
+5. Go to onCameraFrame now and change it as follows:
+
+```java
+        @Override
+        public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+            return inputFrame.rgba();
+        }
+```
+# Now go to Android Manifest to set all the permissions and other parameters
+```java
+<uses-permission android:name="android.permission.CAMERA"/>
+```
+
+# Go to your activity_main.xml and add the following:
+```java
+    <org.opencv.android.JavaCameraView
+        android:layout_height="match_parent"
+        android:layout_width="match_parent"
+        android:id="@+id/java_camera_view"
+        />
+```
+6. Do a Gradle sync and if everything does on perfectly then we are good. Now do a build and then we
+are pretty-much ready to install our app
---
+++ Android_native_tutorial/app/src/main/java/com/example/android_native_tutorial/MainActivity.java
@@ -0,0 +1,133 @@
+package com.example.android_native_tutorial;
+
+// https://stackoverflow.com/questions/55835542/working-with-opencv-in-the-new-android-studio-3-4
+// https://stackoverflow.com/questions/20666366/it-seems-that-your-device-does-not-support-cameraor-it-is-locked
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+import org.opencv.dnn.Net;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    CameraBridgeViewBase cameraBridgeViewBase;
+    BaseLoaderCallback baseLoaderCallback;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        // First check android version
+        //Check if permission is already granted
+        //thisActivity is your activity. (e.g.: MainActivity.this)
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Give first an explanation, if needed.
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        1);
+            }
+        }
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cameraBridgeViewBase = (JavaCameraView)findViewById(R.id.java_camera_view);
+        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        cameraBridgeViewBase.setCvCameraViewListener(this);
+
+        baseLoaderCallback = new BaseLoaderCallback(this) {
+            @Override
+            public void onManagerConnected(int status) {
+                switch (status){
+                    case BaseLoaderCallback.SUCCESS:
+                        cameraBridgeViewBase.enableView();
+                        break;
+                    default:
+                        super.onManagerConnected(status);
+                        break;
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        Mat frame = inputFrame.gray();
+        return frame;
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        try
+        {
+            if (!OpenCVLoader.initDebug()){
+                Toast toast = Toast.makeText(getApplicationContext(), "There is a problem indeed", Toast.LENGTH_LONG);
+                toast.show();
+            }
+            else{
+                baseLoaderCallback.onManagerConnected(baseLoaderCallback.SUCCESS);
+            }
+
+        }
+        catch(Exception e)
+        {
+            String ttt = e.getMessage();
+            int t = 9;
+            //messageBox("doStuff", e.getMessage());
+        }
+
+
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        if (cameraBridgeViewBase != null){
+            cameraBridgeViewBase.disableView();
+        }
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        if (cameraBridgeViewBase != null){
+            cameraBridgeViewBase.disableView();
+        }
+    }
+}
